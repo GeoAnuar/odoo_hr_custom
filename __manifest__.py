50a6e7d87e4ba512d920c4bd432d5aae34780fad
Anuar <github@geometry.kz>
2025-06-30 18:21:05 +0500

    Начальная версия кастомного модуля HR

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'HR Customizations',
+    'version': '1.0',
+    'depends': ['hr', 'hr_recruitment'],
+    'data': [
+        'views/hr_employee_views.xml',         
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models/employee.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class HrEmployee(models.Model):
+    _inherit = 'hr.employee'  # Наследуем от модели сотрудников
+     
+    iin = fields.Char(string='ИИН')
+    contract_number = fields.Char(string='№ Трудового договора')
+    hiring_date = fields.Date(string='Дата устройства')
+    internal_department = fields.Many2one('hr.department', string='Отдел')
+    internal_job_title = fields.Many2one('hr.job', string='Должность по внутренней структуре')
+    
+
+   
