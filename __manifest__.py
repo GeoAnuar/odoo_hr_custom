841b0d233b039e91a9ba3fa28cbafcfb99e68c61
Anuar <github@geometry.kz>
2025-07-04 10:19:10 +0500

    Внесены кастомные стадии принятия

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,9 @@
     'version': '1.0',
     'depends': ['hr', 'hr_recruitment'],
     'data': [
-        'views/hr_employee_views.xml',         
+        'views/hr_employee_views.xml',
+        'data/delete_default_stages.xml',  
+        'data/stage_data.xml',       
     ],
     'installable': True,
     'application': True,
